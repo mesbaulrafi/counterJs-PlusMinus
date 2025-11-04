d88cd092a6b9004ebfd0690d674f8c13fbd91d7a
Your Name <you@example.com>
2025-11-04 22:29:45 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,52 @@
+
+// Counter Start
+let minusBtn = document.getElementById("minusBtn");
+let plusBtn = document.getElementById("plusBtn");
+let ruselt = document.getElementById("ruselt");
+
+let alartOne = document.getElementById("alartOne");
+let alartTwo = document.getElementById("alartTwo");
+
+plusBtn.addEventListener("click",()=>{
+    if (ruselt.innerText < 5) {
+        ruselt.innerText++
+        alartOne.setAttribute("hidden", );
+        alartTwo.setAttribute("hidden", true);
+    } else {
+        alartOne.removeAttribute("hidden");
+        alartTwo.setAttribute("hidden", ture);
+    }
+})
+
+minusBtn.addEventListener("click",()=>{
+    if (ruselt.innerText > 0) {
+        ruselt.innerText--
+        alartOne.setAttribute("hidden",true);
+        alartTwo.setAttribute("hidden",);
+    } else {
+        alartOne.setAttribute("hidden",true);
+        alartTwo.removeAttribute("hidden",);
+    }
+})
+
+// Counter End
+
+// counter up part start
+let numberBox = document.querySelectorAll(".numberBox");
+let arr = Array.from(numberBox);
+
+arr.map((item)=>{
+    let counter = 0;
+    function count() {
+        counter++;
+        item.innerHTML = counter;
+        if (counter == item.dataset.a) {
+            clearInterval(stop);
+        }
+    } 
+    let stop = setInterval(function () {
+        count();
+    }, 1000 / item.dataset.a);
+});
+
+// counter up part End
